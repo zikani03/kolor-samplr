5604db82b7aa36328ec8e9e66e14f1330cd1961f
Zikani <zikani.nmwase@ymail.com>
2014-08-30 18:01:30 +0200

    add jquery,uikit scripts to requirejs paths config
    Removed the script tags for loading jQuery and the uiKit scripts from the
    html in favor of having them loaded via requirejs

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 require.config({
 	paths: {
-		ko: '../bower_components/knockout-3.2.0'
-	}
+		"ko" : "../bower_components/knockout-3.2.0",
+		"jquery" : "../bower_components/jquery-1.9.1.min",
+		"uikit"  : "../bower_components/uikit-2.9.0/js/uikit.min"
+	},
+    shim: {
+        "uikit": { deps: ["jquery"] }
+    }
 });
 
 require(['ko','WorkspaceViewModel'], function(ko, WorkspaceViewModel){
         
-	var app = new WorkspaceViewModel(document.getElementById('canvasItem'));
+	var app = new WorkspaceViewModel(document.getElementById('canvasItem'), './img/tumblr_n9ca6yN7X71qz6f9yo1_500.jpg');
 	
 	ko.applyBindings(app);
 	
