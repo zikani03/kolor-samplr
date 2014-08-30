d9c327877bd9cf27e98ec8a9330206c7d60514cf
Zikani <zikani.nmwase@ymail.com>
2014-08-30 17:52:10 +0200

    Fix requirejs dependency
    KsMarker was depending on Color instead of ColorSample in the define

--- js/KsMarker.js
+++ js/KsMarker.js
@@ -1,4 +1,4 @@
-define(['ko', 'Color'], function(ko, ColorSample) {
+define(['ko', 'ColorSample'], function(ko, ColorSample) {
     
     function KsMarker(sample) {
         this.visible = ko.observable(true);
