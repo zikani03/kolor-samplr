44fd6abe536c323a4afdff5931a1398e03e27d2f
Zikani <zikani.nmwase@ymail.com>
2014-08-31 11:02:15 +0200

    Register component in app.js, update requirejs config
    Set the baseUrl for the requirejs config.
    
    Removed code that referenced the WorkspaceViewModel directly since we're
    using components now :)

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
 require.config({
+    baseUrl: ".",
 	paths: {
-		"ko" : "../bower_components/knockout-3.2.0",
-		"jquery" : "../bower_components/jquery-1.9.1.min",
-		"uikit"  : "../bower_components/uikit-2.9.0/js/uikit.min"
+		"ko" : "bower_components/knockout-3.2.0",
+		"jquery" : "bower_components/jquery-1.9.1.min",
+		"uikit"  : "bower_components/uikit-2.9.0/js/uikit.min",
+		"text"  : "bower_components/require.text",
+        
+        "ColorSample" : "js/ColorSample",
+        "KsMarker" : "js/KsMarker"
 	},
     shim: {
         "uikit": { deps: ["jquery"] }
     }
 });
 
-require(['ko','WorkspaceViewModel'], function(ko, WorkspaceViewModel){
-        
-	var app = new WorkspaceViewModel(document.getElementById('canvasItem'), './img/tumblr_n9ca6yN7X71qz6f9yo1_500.jpg');
+require(['ko', 'jquery', 'uikit'], function(ko){
+    ko.components.register("ks-workspace", { require: "components/workspace/Workspace" });
+    
+	//var app = new WorkspaceViewModel(document.getElementById('canvasItem'), './img/medium_14889020083.jpg');
 	
-	ko.applyBindings(app);
+	ko.applyBindings();
 	
 	var requestAnimFrame = (function(){
 		window.requestAnimationFrame       ||
