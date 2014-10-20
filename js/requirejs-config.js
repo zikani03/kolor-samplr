10273654dce2a27d43e15669654a51002b54260c
Zikani <zikani.nmwase@ymail.com>
2014-10-20 23:01:43 +0200

    Update bower.json and requirejs config

--- js/requirejs-config.js
+++ js/requirejs-config.js
@@ -4,7 +4,7 @@ var require = {
 		"ko"        : "bower_components/knockoutjs/dist/knockout",
 		"text"      : "bower_components/text/text",
 		"jquery"    : "bower_components/jquery-1.11.1.min/index",
-		"jquery.ui" : "bower_components/jquery-ui",
+		"jquery.ui" : "bower_components/jquery-ui/jquery-ui.min",
 		"uikit"     : "bower_components/uikit-2.9.0/js/uikit.min"
 	},
     shim: {
