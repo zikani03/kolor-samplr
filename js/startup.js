9af118508cb4fb409f9b1a6187e3ca41dd721fc7
Zikani <zikani.nmwase@ymail.com>
2014-10-20 22:26:56 +0200

    Split requirejs config and app startup
    separated the requirejs config and the application startup scripts, to
    keep things cleaner (copied the approach from knockout-triage)

--- js/startup.js
+++ js/startup.js
@@ -1,27 +1,9 @@
-require.config({
-    baseUrl: ".",
-	paths: {
-		"ko" : "bower_components/knockoutjs/dist/knockout",
-		"text"  : "bower_components/text/text",
-        
-		"jquery" : "bower_components/jquery-1.11.1.min/index",
-		"uikit"  : "bower_components/uikit-2.9.0/js/uikit.min",
-        
-        "ColorSample" : "js/ColorSample",
-        "KsMarker" : "js/KsMarker"
-	},
-    shim: {
-        "uikit": { deps: ["jquery"] }
-    }
-});
-
 require(['ko', 'jquery', 'uikit'], function(ko){
     ko.components.register("ks-workspace", { require: "components/workspace/Workspace" });
     
 	//var app = new WorkspaceViewModel(document.getElementById('canvasItem'), './img/medium_14889020083.jpg');
-	
 	ko.applyBindings();
-	
+
 	var requestAnimFrame = (function(){
 		window.requestAnimationFrame       ||
              window.webkitRequestAnimationFrame ||
