8c5b3a24a66e7786c9fc10695034bef00c695adb
Zikani Nyirenda Mwase <zikani@creditdatamw.com>
2015-07-06 23:20:20 +0200

    Load component, jquery, remove rAF polyfill
    Updated startup to load register the component as an object and not using
    require.
    No longer using the request animation polyfill in the app
    finally, loading jquery

--- src/app/startup.js
+++ src/app/startup.js
@@ -1,21 +1,12 @@
+'use strict';
+import $ from 'jquery';
 import ko from 'knockout';
+import Component from './workspace/index.js';
 import Application from './application.js';
 
-ko.components.register("ks-workspace", { require: "workspace/index" });
-
-// request animation frame polyfill - do we still need this?
-(function(){
-    window.requestAnimationFrame       ||
-         window.webkitRequestAnimationFrame ||
-         window.mozRequestAnimationFrame    ||
-         window.oRequestAnimationFrame      ||
-         window.msRequestAnimationFrame     ||
-         function( callback ){
-           window.setTimeout( callback, 1000/60);
-         };
-})();
-
 $(function() {
+    ko.components.register("ks-workspace", Component);
+    
     var app = new Application();
     app.addWorkspace('/img/medium_14889020083.jpg');    
 	ko.applyBindings(app);
